42e91cb0bf7157a9a507d8420addbc43bd2fa19b
ANTHONUS <anthonus.d@gmail.com>
2025-06-19 16:17:42 +0200

    changement fonction getCoordonee

--- notepad/notepad.cpp
+++ notepad/notepad.cpp
@@ -123,6 +123,8 @@ void Notepad::open() {
             lonD, lonM, WE);
     }
 
+    std::cout << lastParcours->getEtape(0)->getCoordonnee().toStdString() << std::endl;
+
     ui->numParcours->setMaximum(parcoursList.size());
     ui->numParcours->setValue(parcoursList.size());
     ui->numEtape->setMaximum(lastParcours->getNombreEtapes());
